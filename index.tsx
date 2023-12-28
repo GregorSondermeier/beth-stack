ca2bb98ee4226e504314adf2021350f372b884e8
Gregor Sondermeier <g.sondermeier@acker.co>
2023-12-28 11:31:46 +0100

    install typed-html and render jsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,21 @@
 import { Elysia } from 'elysia';
 import { html } from '@elysiajs/html';
+import * as elements from 'typed-html';
 
 const app = new Elysia()
   .use(html())
-  .get('/', ({ html }) => html(baseHtml))
+  .get('/', ({ html }) => html(
+    <BaseHtml>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </BaseHtml>)
+  )
   .listen(3000);
 
 console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
 
-const baseHtml = `
+const BaseHtml = ({ children }: elements.Children) => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,8 +23,8 @@ const baseHtml = `
   <meta name="viewport" content="width=device-with, initial-scale=1.0">
   <title>THE BETH STACK</title>
 </head>
-<body>
-I'm a html document
-</body>
+
+${children}
+
 </html>
 `;
